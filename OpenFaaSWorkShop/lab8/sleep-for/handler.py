92653ea356a42c9464bc7c74a188c596bd7d31f9
takmnagaya <takmnagaya@gmail.com>
2018-09-28 13:04:27 +0900

    check timeout

--- OpenFaaSWorkShop/lab8/sleep-for/handler.py
+++ OpenFaaSWorkShop/lab8/sleep-for/handler.py
@@ -1,7 +1,14 @@
+import time
+import os
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
 
-    return req
+    sleep_duration = int(os.getenv("sleep_duration", "10"))
+    preSleep = "Starting to sleep for %d" % sleep_duration
+    time.sleep(sleep_duration)  # Sleep for a number of seconds
+    postSleep = "Finished the sleep"
+    return preSleep + "\n" + postSleep
