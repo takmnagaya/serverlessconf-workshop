9895ac6d60cff5b1e58cd263c10f5d19a85dd69c
takmnagaya <takmnagaya@gmail.com>
2018-09-28 11:59:12 +0900

    polarity check

--- OpenFaaSWorkShop/lab5/issue-bot/handler.py
+++ OpenFaaSWorkShop/lab5/issue-bot/handler.py
@@ -1,7 +1,24 @@
-def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
+import requests, json, os, sys
 
-    return req
+def handle(req):
+
+    event_header = os.getenv("Http_X_Github_Event")
+
+    if not event_header == "issues":
+        sys.exit("Unable to handle X-GitHub-Event: " + event_header)
+        return
+
+    gateway_hostname = os.getenv("gateway_hostname", "gateway")
+
+    payload = json.loads(req)
+
+    if not payload["action"] == "opened":
+        return
+
+    #sentimentanalysis
+    res = requests.post('http://' + gateway_hostname + ':8080/function/sentimentanalysis', data=payload["issue"]["title"]+" "+payload["issue"]["body"])
+
+    if res.status_code != 200:
+        sys.exit("Error with sentimentanalysis, expected: %d, got: %d\n" % (200, res.status_code))
+
+    return res.json()
